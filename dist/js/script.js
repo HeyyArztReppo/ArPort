095555c914b9c6dfb310aa97a645a692136fa0e1
HeyyArztReppo <heyyjustmee@gmail.com>
2024-01-14 14:52:01 +0700

    Final launching

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,7 +2,7 @@
 window.onscroll = function() {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
-    const toTop = document.querySelector('#to-top')
+    const toTop = document.querySelector('#to-top');
 
     if(window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
